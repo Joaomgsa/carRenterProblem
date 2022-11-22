a186fb0330dfd36d3afe6f04f4b2c810be1a9fb7
Joaomgsa <joaomgsa@gmail.com>
2022-11-21 21:56:04 -0300

    Ajustando entrada da string

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,9 +38,9 @@ public class Program {
         rentalService.processInvoice(cr);
 
         System.out.println("FATURA");
-        System.out.println("Pagamento Basico :"+ cr.getInvoice().getBasicPayment());
-        System.out.println("Imposto :"+ cr.getInvoice().getTax());
-        System.out.println("Pagamento Total :"+ cr.getInvoice().getTotalPayment());
+        System.out.println("Pagamento Basico :"+ String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Imposto :"+ String.format("%.2f",cr.getInvoice().getTax()));
+        System.out.println("Pagamento Total :"+ String.format("%.2f",cr.getInvoice().getTotalPayment()));
 
         sc.close();
     }
